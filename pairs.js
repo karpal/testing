c7a043cda1753f48004c4f60b0b22c340c156fe3
karpal <ilham.nurdian84@gmail.com>
2025-04-19 10:04:20 +0800

    Create pairs.js

---
+++ pairs.js
@@ -0,0 +1,91 @@
+// pairs.js
+
+const tokenPairs = [
+  {
+    baseToken: {
+      symbol: "sMON",
+      address: "0xe1d2439b75fb9746e7bc6cb777ae10aa7f7ef9c5"
+    },
+    quoteToken: {
+      symbol: "WETH",
+      address: "0xb5a30b0fdc5ea94a52fdc42e3e9760cb8449fb37"
+    }
+  },
+  {
+    baseToken: {
+      symbol: "USDC",
+      address: "0xf817257fed379853cde0fa4f97ab987181b1e5ea"
+    },
+    quoteToken: {
+      
+    symbol: "sMON",
+      address: "0xe1d2439b75fb9746e7bc6cb777ae10aa7f7ef9c5"
+    }
+  },
+  {
+    baseToken: {
+      symbol: "USDT",
+      address: "0x88b8e2161dedc77ef4ab7585569d2415a1c1055d"
+    },
+    quoteToken: {
+      
+    symbol: "sMON",
+      address: "0xe1d2439b75fb9746e7bc6cb777ae10aa7f7ef9c5"
+    }
+  },
+  {
+    baseToken: {
+      symbol: "WETH",
+      address: "0xb5a30b0fdc5ea94a52fdc42e3e9760cb8449fb37"
+    },
+    quoteToken: {
+      
+    symbol: "sMON",
+      address: "0xe1d2439b75fb9746e7bc6cb777ae10aa7f7ef9c5"
+    }
+  },
+  {
+    baseToken: {
+      symbol: "USDC",
+      address: "0xf817257fed379853cde0fa4f97ab987181b1e5ea"
+    },
+    quoteToken: {
+      symbol: "USDT",
+      address: "0x88b8e2161dedc77ef4ab7585569d2415a1c1055d"
+    }
+  },
+  {
+    baseToken: {
+      symbol: "USDT",
+      address: "0x88b8e2161dedc77ef4ab7585569d2415a1c1055d"
+    
+    },
+    quoteToken: {
+      symbol: "USDC",
+      address: "0xf817257fed379853cde0fa4f97ab987181b1e5ea"
+    }
+  },
+  {
+    baseToken: {
+      symbol: "WBTC",
+      address: "0xcf5a6076cfa32686c0df13abada2b40dec133f1d"
+    
+    },
+    quoteToken: {
+      symbol: "WETH",
+      address: "0xb5a30b0fdc5ea94a52fdc42e3e9760cb8449fb37"
+    }
+  },
+  {
+    baseToken: {
+      symbol: "WETH",
+      address: "0xb5a30b0fdc5ea94a52fdc42e3e9760cb8449fb37"
+    },
+    quoteToken: {
+      symbol: "WBTC",
+      address: "0xcf5a6076cfa32686c0df13abada2b40dec133f1d"
+    }
+  }
+];
+
+module.exports = tokenPairs;
